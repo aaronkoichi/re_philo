03e4f709a8eb3736fc6f4f606cc28238df2e3df4
Zhe Tse Lee <zlee@u81z05s04.42kl.edu.my>
2025-07-11 19:11:09 +0800

    whoops norm

--- philo/philo_num_check.c
+++ philo/philo_num_check.c
@@ -6,7 +6,7 @@
 /*   By: zlee <zlee@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:55:00 by zlee              #+#    #+#             */
-/*   Updated: 2025/07/11 18:59:05 by zlee             ###   ########.fr       */
+/*   Updated: 2025/07/11 19:10:55 by zlee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	valid_checker(char *str)
 	int	i;
 
 	i = -1;
-	while(str[++i])
+	while (str[++i])
 		if (str[i] < '0' || str[i] > '9')
 			return (0);
 	return (1);
